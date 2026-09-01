44bdaf53b2580e2b1e03f09188eb9f2c731649cc
WESLEY DEL SOTO DA ROSA <wesleydelsotodarosa@gmail.com>
2026-09-01 10:39:46 -0300

    Exercicios 8 a 10

--- Exercicios guanabara/Exercicios009.py
+++ Exercicios guanabara/Exercicios009.py
@@ -1,1 +1,6 @@
 #Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+t = int(input("Digite um número "))
+print ("=" *20)
+print (f"{t} x {1:2} = {t*1}\n{t} x {2:2} = {t*2}\n{t} x {3:2} = {t*3}\n{t} x {4:2} = {t*4}\n{t} x {5:2} = {t*5}")
+print (f"{t} x {6:2} = {t*6}\n{t} x {7:2} - {t*7}\n{t} x {8:2} = {t*8}\n{t} x {9:2} = {t*9}\n{t} x {10:2} = {t*10}")
+print ("=" *20)
--- Exercicios guanabara/Exercicios010.py
+++ Exercicios guanabara/Exercicios010.py
@@ -1,2 +1,5 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre 
 # quantos dólares ela pode comprar, considere : 1,00 = 3,27
+c = float(input("Quanto dinheiro você tem n carteira? R$"))
+d = c/3.27
+print (f"Com R${c:.2f} você pode comprar U${d:.2f}")
