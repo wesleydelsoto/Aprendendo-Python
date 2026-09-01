e6c44df91bad478cb396fd274400785e217e2f71
WESLEY DEL SOTO DA ROSA <wesleydelsotodarosa@gmail.com>
2026-09-01 11:06:57 -0300

    Exercicios 11 e 12

--- Exercicios guanabara/Exercicios011.py
+++ Exercicios guanabara/Exercicios011.py
@@ -1,3 +1,9 @@
 #Faça um programa que leia a altura e a largura de uma parede em metros, calcule sua area
 # e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta
 # pinta uma area de 2m².
+l = float(input("Digite a largura da parede "))
+a = float(input("Digite a altura da parede "))
+ar = l*a
+tinta = ar/2
+print (f"Sua parede tem a dimensão {l}x{a} e sua área é de {ar}m²")
+print (f"Para pinta-la precisara de {tinta}l de tinta.")
--- Exercicios guanabara/Exercicios012.py
+++ Exercicios guanabara/Exercicios012.py
@@ -1,2 +1,6 @@
-#Faça um algoritmo que leia o preço de um produto e mostre seu nov preço com 
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 
 #5% de desconto.
+p = float(input("Digite o preço do produto R$ "))
+d = p * 0.05
+pf = p - d
+print (f" O produto que custava {p} R$ com desconto custa {pf:.2f} R$")
