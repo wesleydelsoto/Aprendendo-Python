72ced1a8746451b405728c4a5164b0ae54a87f26
WESLEY DEL SOTO DA ROSA <wesleydelsotodarosa@gmail.com>
2026-09-01 13:30:29 -0300

    Exercicio 13

--- Exercicios guanabara/Exercicios013.py
+++ Exercicios guanabara/Exercicios013.py
@@ -1,2 +1,6 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo
 # salário com 15% de aumento
+sal = float(input("Qual o salário do funcionário? "))
+au = sal * 0.15
+nsa = sal + au
+print (f"O salário que era {sal} com o aumento ficou {nsa}")
