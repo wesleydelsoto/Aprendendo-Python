0f431a07467fbf97dc0932044a90a5b8c955bdf7
WESLEY DEL SOTO DA ROSA <wesleydelsotodarosa@gmail.com>
2026-09-02 20:05:02 -0300

    exercicio 16/17

--- Exercicios guanabara/Exercicios017.py
+++ Exercicios guanabara/Exercicios017.py
@@ -1,2 +1,7 @@
 # Faça um progrma que leia o comprimento do cateto oposto e do cateto adjacente de um traingulo retangulo
 # calcule e mostre o comprimento da hipotenusa.
+import math
+ca1 = float(input("Qual o comprimento do cateto adjacente? "))
+ca2 = float(input("Qual o comprimento do cateto oposto? "))
+hi = math.hypot(ca1,ca2)
+print (f" A hipotenusa entre {ca1} e {ca2} é {hi}")
