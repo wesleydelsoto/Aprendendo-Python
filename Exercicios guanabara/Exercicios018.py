58be2c2951fb1fa47af905466fa45f13006e0b91
WESLEY DEL SOTO DA ROSA <wesleydelsotodarosa@gmail.com>
2026-09-03 19:38:41 -0300

    Exercicio 18

--- Exercicios guanabara/Exercicios018.py
+++ Exercicios guanabara/Exercicios018.py
@@ -1,2 +1,5 @@
 # Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno,cosseno
 # e tangente deste angulo.
+import math
+t = int(input("Digite o ângulo: "))
+print (f" O seno, cosseno e o tangente de {t} é {math.tan(t)}")
