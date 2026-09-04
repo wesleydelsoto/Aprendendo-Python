0d7331e0753dcf91777db920d8107dafd0450351
WESLEY DEL SOTO DA ROSA <wesleydelsotodarosa@gmail.com>
2026-09-04 13:33:18 -0300

    Exercicios 16 e 17

--- Exercicios guanabara/Exercicios016.py
+++ Exercicios guanabara/Exercicios016.py
@@ -1,5 +1,5 @@
 # Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção
 # inteira EX: Digite um número: 6.127 o número 6.127 tem a parte inteira 6 #
-import math
+from math import trunc
 num = float(input("Digite um número: "))
-print (f" O número {num} tem a parte {math.trunc(num)}")
+print (f" O número {num} tem a parte {trunc(num)}")
